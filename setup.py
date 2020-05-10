ce48641a26baac669a4cc1140f8d557b3f09e3ea
Nitish-Sharma01 <30517848+Nitish-Sharma01@users.noreply.github.com>
2020-05-10 18:32:08 +0530

    Create setup.py
    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.0.1'
+PACKAGE_NAME = 'GPD'
+AUTHOR = 'Nitish Sharma'
+AUTHOR_EMAIL = 'nitish.sharma0712@gmail.com'
+URL = 'https://github.com/Nitish-Sharma01/pwmdist'
+
+LICENSE = 'Apache License 2.0'
+DESCRIPTION = 'package for essential statistics of extreme value distirbutions using probability weighted moments'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'numpy',
+      'pandas',
+      'statistics',
+      'scipy',
+      'matplotlib',
+      'random',
+      'seaborn'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )
