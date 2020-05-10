f85427272734e11bd6dd8a82a1d0458094dfa4aa
Nitish-Sharma01 <30517848+Nitish-Sharma01@users.noreply.github.com>
2020-05-10 21:15:32 +0530

    Update __init__.py

--- GPD/__init__.py
+++ GPD/__init__.py
@@ -1,3 +1,10 @@
 from . import (
     gpdpwmFit, gpdplots
     )
+def _get_version_from_setuptools():
+    from pkg_resources import get_distribution
+    return get_distribution("GPD").version
+
+
+__all__ = ['__version__']
+__version__ = _get_version_from_setuptools()
