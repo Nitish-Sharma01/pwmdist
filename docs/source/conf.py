feea2fa25e8f5eef631988350c9ad67db31a6f15
Nitish-Sharma01 <30517848+Nitish-Sharma01@users.noreply.github.com>
2020-05-11 20:55:28 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.append('C:\\Users\\Admin\\Desktop\\pwmdist\\pwmdist\\pwmdist')
+sys.path.insert(0, os.path.abspath('../../'))
 
 
 # -- Project information -----------------------------------------------------
